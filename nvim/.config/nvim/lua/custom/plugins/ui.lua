c26c07f64c69ab21ccecdc70346ea4b40588b62e
zak warsame <zakwarsamee@gmail.com>
2024-10-14 23:41:20 -0400

    mini icons

--- nvim/.config/nvim/lua/custom/plugins/ui.lua
+++ nvim/.config/nvim/lua/custom/plugins/ui.lua
@@ -153,6 +153,7 @@ return {
       -- - sd'   - [S]urround [D]elete [']quotes
       -- - sr)'  - [S]urround [R]eplace [)] [']
       require('mini.surround').setup()
+      require('mini.icons').setup()
     end,
   },
   {
