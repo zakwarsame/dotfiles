890f2534781eb17898d300b3015bb05d0c640f46
Zakaria Warsame <zakaria.warsame@shopify.com>
2025-03-08 19:56:24 -0500

    emojis lol

--- nvim/.config/nvim/lua/custom/which-key.lua
+++ nvim/.config/nvim/lua/custom/which-key.lua
@@ -4,12 +4,12 @@ return {
     local wk = require 'which-key'
     wk.add {
       -- [LEADER] mappings
-      { '<leader>c', icon = ' ', group = '[C]ode' },
+      { '<leader>c', icon = '󰆩 ', group = '[C]ode' },
       { '<leader>d', icon = '󰈙 ', group = '[D]ocument' },
       { '<leader>r', icon = '󰑕 ', group = '[R]ename' },
-      { '<leader>s', icon = ' ', group = '[S]earch' },
-      { '<leader>w', icon = ' ', group = '[W]orkspace' },
-      { '<leader>t', icon = ' ', group = '[T]oggle' },
+      { '<leader>s', icon = '󰍉 ', group = '[S]earch' },
+      { '<leader>w', icon = '󱂬 ', group = '[W]orkspace' },
+      { '<leader>t', icon = '󰔡 ', group = '[T]oggle' },
       {
         mode = { 'n', 'v' }, -- NORMAL and VISUAL mode
         { '<leader>h', icon = '󰊢 ', desc = 'Git [H]unk' },
